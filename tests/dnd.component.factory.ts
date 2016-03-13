9b6103c3f5cf84bfba14763aa220409a74873e5b
Sergey Akopkokhyants <akserg@gmail.com>
2016-03-13 21:09:43 +0200

    Added first unit tests

---
+++ tests/dnd.component.factory.ts
@@ -0,0 +1,57 @@
+import {Component, Input, Output, EventEmitter, ElementRef} from 'angular2/core';
+
+import {DraggableComponent} from '../src/dnd.draggable';
+import {DroppableComponent} from '../src/dnd.droppable';
+
+@Component({ 
+  selector: 'container',
+  template: `
+<div id='dragIdOne' dnd-draggable [dropZones]="['zone-one']" (onDragSuccess)="dragOneSuccessCallback($event)"></div>
+<div id='dragIdTwo' dnd-draggable [dropZones]="['zone-two']" (onDragSuccess)="dragTwoSuccessCallback($event)"></div>
+<div id='dragIdOneTwo' dnd-draggable [dropZones]="['zone-one', 'zone-two']" (onDragSuccess)="dragOneTwoSuccessCallback($event)"></div>
+
+<div id='dropIdOne' dnd-droppable [dropZones]="['zone-one']" (onDropSuccess)="dropOneSuccessCallback($event)"></div>
+<div id='dropIdTwo' dnd-droppable [dropZones]="['zone-two']" (onDropSuccess)="dropTwoSuccessCallback($event)"></div>
+<div id='dropIdOneTwo' dnd-droppable [dropZones]="['zone-one', 'zone-two']" (onDropSuccess)="dropOneTwoSuccessCallback($event)"></div>
+`,
+  directives: [DraggableComponent, DroppableComponent]
+})
+export class Container {  
+    @Output() dragOne:EventEmitter<any> = new EventEmitter<any>();
+    @Output() dragTwo:EventEmitter<any> = new EventEmitter<any>();
+    @Output() dragOneTwo:EventEmitter<any> = new EventEmitter<any>();
+    
+    @Output() dropOne:EventEmitter<any> = new EventEmitter<any>();
+    @Output() dropTwo:EventEmitter<any> = new EventEmitter<any>();
+    @Output() dropOneTwo:EventEmitter<any> = new EventEmitter<any>();
+    
+    private dragOneSuccessCallback($event:any) {
+        this.dragOne.emit($event);
+    }
+    
+    private dragTwoSuccessCallback($event:any) {
+        this.dragOne.emit($event);
+    }
+    
+    private dragOneTwoSuccessCallback($event:any) {
+        this.dragOneTwo.emit($event);
+    }
+    
+    private dropOneSuccessCallback($event:any) {
+        this.dropOne.emit($event);
+    }
+    
+    private dropTwoSuccessCallback($event:any) {
+        this.dropTwo.emit($event);
+    }
+    
+    private dropOneTwoSuccessCallback($event:any) {
+        this.dropOneTwo.emit($event);
+    }
+}
+
+export function triggerEvent(elem:HTMLElement, eventName:string, eventType:string) {
+    var event:Event = document.createEvent(eventType);
+    event.initEvent(eventName, true, true);
+    elem.dispatchEvent(event);
+} 
--- tests/dnd.draggable.spec.ts
+++ tests/dnd.draggable.spec.ts
@@ -20,31 +20,52 @@ import {Observable} from 'rxjs/Observable';
 import {DraggableComponent} from '../src/dnd.draggable';
 import {DragDropService} from '../src/dnd.service';
 
+import {Container, triggerEvent} from './dnd.component.factory';
+
 export function main() {
     describe('Drag and Drop', () => {
 
         let componentFixture: ComponentFixture;
         let dragdropService: DragDropService;
         
-        let draggableData:any = {
-            
-        };
-
         beforeEachProviders(() => {
             return [TEST_BROWSER_PLATFORM_PROVIDERS, TEST_BROWSER_APPLICATION_PROVIDERS, DragDropService];
         });
 
         beforeEach(injectAsync([TestComponentBuilder, DragDropService], (tcb: TestComponentBuilder, dd: DragDropService) => {
             dragdropService = dd;
-            return tcb.createAsync(DraggableComponent).then((cf: ComponentFixture) => {
+            return tcb.createAsync(Container).then((cf: ComponentFixture) => {
                 componentFixture = cf;
-                componentFixture.componentInstance.draggableData = draggableData;
+                // componentFixture.componentInstance.draggableData = draggableData;
                 componentFixture.detectChanges();
             });
         }));
 
         it('should be defined', () => {
-            // expect(componentFixture).toBeDefined();
+            expect(componentFixture).toBeDefined();
+            console.log(componentFixture.nativeElement);
+        });
+        
+        it('Drop events should not be activated on the wrong drop-zone', (done:any) => {
+           let dragElemOne:HTMLElement = componentFixture.nativeElement.querySelector('#dragIdOne'); 
+           let dropElemTwo:HTMLElement = componentFixture.nativeElement.querySelector('#dropIdTwo');
+           
+           triggerEvent(dragElemOne, 'dragstart', 'MouseEvent');
+           triggerEvent(dropElemTwo, 'dragenter', 'MouseEvent');
+           componentFixture.detectChanges();
+           expect(dropElemTwo).not.toHaveCssClass(dragdropService.onDragEnterClass);
+           
+           triggerEvent(dropElemTwo, 'dragover', 'MouseEvent');
+           componentFixture.detectChanges();
+           expect(dropElemTwo).not.toHaveCssClass(dragdropService.onDragOverClass);
+           
+           expect((<Container>componentFixture.componentInstance).dragOne.count).toBe(0);
+        //    (<Container>componentFixture.componentInstance).
+        //    triggerEvent(dragElemOne, 'drop', 'MouseEvent');
+        //    expect(scope.context['dragOneSuccessCallback']).not.toHaveBeenCalled();
+        //    expect(scope.context['dropTwoSuccessCallback']).not.toHaveBeenCalled();
+           
+           done();
         });
     });
 }
