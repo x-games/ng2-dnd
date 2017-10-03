f965e44a13461fa5a0d465778a5728cf72564752
Sergey Akopkokhyants <akserg@gmail.com>
2017-10-03 10:04:43 +0200

    Fixed NPE on Property 'destroyed' does not exist on type 'ChangeDetectorRef'

--- src/abstract.component.ts
+++ src/abstract.component.ts
@@ -193,9 +193,7 @@ export abstract class AbstractComponent {
     detectChanges() {
         // Programmatically run change detection to fix issue in Safari
         setTimeout(() => {
-            if (!this._cdr.destroyed) {
-                this._cdr.detectChanges();
-            }
+            this._cdr && this._cdr.detectChanges();
         }, 250);
     }
 
